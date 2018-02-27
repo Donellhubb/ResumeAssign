c11ce568ce49ac8b833975b71192075b3ba4df2c
syntel <syntel@PHX440G3-2815YX.syntelorg.com>
2018-02-27 11:13:36 -0500

    adding resume

---
+++ practice/src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        System.out.println("Group \"There Are 4 Letters in Java\" Introductions:" + "\n");
+
+        System.out.println("About Shahar Levy:");
+        System.out.println("Education: Shahar gaduated from the University of Georgia with a degree in Biochemical Engineering.");
+        System.out.println("Significant work experience: Shahar worked as an engineer for a polymer company " + "\n" +
+                "where he worked on developing and characterizing new polymers.");
+        System.out.println("Skills: Shahar's best skill is his ability to ask questions and take initiative to find out more." + "\n");
+
+        System.out.println("About Tahrim Uddin:");
+        System.out.println("Education: Tahrim graduated from Florida Atlantic University with a degree in Biology.");
+        System.out.println("Significant work experience: Tahrim worked as an SEO analyst for a local website in West" + "\n" +
+                "Palm Beach, Fl using Google Analytics and Google Webmaster Tools.");
+        System.out.println("Skills: Tahrim has experience working with Ruby on Rails and C++." + "\n");
+
+        System.out.println("About Bob Igbarumah:");
+        System.out.println("Education: Bob graduated from the University of Texas at Arlington with a degree in Electrical Engineering");
+        System.out.println("Significant work experience: Bob built web apps using Python and Django.");
+        System.out.println("Skills: Bob has experience using Python, Django, Javascript, and Java" + "\n");
+
+        System.out.println("About Donell Hubbard:");
+        System.out.println("Education: Donell graduated from The Tuskegee University with a B.S. degree in Mechanical Engineering");
+        System.out.println("Significant work experience: Donell worked for Raytheon Missle Systems where he improved" + "\n" +
+                "missle processes. Previously, he interned for NASA and worked on the SLS Rocket.");
+        System.out.println("Move from Tucson, AZ to Atlanta, GA");
+        System.out.println("Skills: Donell is proficient in AutoCAD, Creo, Solidworks, and other CAD software." + "\n");
+
+        System.out.println("About Robert Kee:");
+        System.out.println("Education: Robert graduated from Georgia State with a Computer Science degree.");
+        System.out.println("Significant work experience: Robert worked in desktop support IT for 5 years at a commercial" + "\n" +
+                "art school.");
+        System.out.println("Skills: Robert has worked with C/C++, Python and Java." + "\n");
+
+        System.out.println("About John Hill:");
+        System.out.println("Education: Hill graduated from Georgia State University with a Computer Information Systems degree.");
+        System.out.println("Significant work experience: He worked as an IT developer with Hewlett Packard Enterprises under " + "\n" +
+                "the Centers of Excellence Big Data Analytics & Consumption Tools team where he onboarded tenants to the new platform." + "\n" +
+                "He also created the UI for tenants to access resources specific to the tools.");
+        System.out.println("Skills: Hill has worked with HTML, CSS, Javascript, Twitter BootStrap, Foundation, Node.js, Angular.js, MongoDB, " + "\n" +
+                "and deployed through Heroku");
+
+    }
+}
